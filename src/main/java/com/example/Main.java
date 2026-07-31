02b605ecdfcb5289dbd003374f6b5d5007825558
juanda-mora <juan.mora@usantoto.edu.co>
2026-07-31 16:51:06 -0500

    Creacion de las clases base

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,7 +4,7 @@ import com.example.Services.ProductoService;
 
 public class Main {
     public static void main(String[] args) {
-        ProductoService productoService = new ProductoService();
+        
         
     }
 }
