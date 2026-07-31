f89f03f19fe92b65c116802f9c36a8fb8829d995
Juan David Mora Puentes <juan.mora@usantoto.edu.co>
2026-07-31 17:40:47 -0500

    Add method to update existing product

--- src/main/java/com/example/Services/ProductoService.java
+++ src/main/java/com/example/Services/ProductoService.java
@@ -29,6 +29,22 @@ public class ProductoService {
         return null; 
     }
 
+    public boolean actualizarProducto(Productos productoActualizado) {
+
+    if (productoActualizado.getPrecio() < 0) {
+        throw new IllegalArgumentException("El precio no puede ser negativo");
+    }
+
+    for (int i = 0; i < productos.size(); i++) {
+        if (productos.get(i).getIdProducto() == productoActualizado.getIdProducto()) {
+            productos.set(i, productoActualizado);
+            return true;
+        }
+    }
+
+    return false;
+}
+
     public ArrayList<Productos> obtenerTodosLosProductos() {
         return productos;
     }
